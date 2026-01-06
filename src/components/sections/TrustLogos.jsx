d99595343feea24191315c20f6042fa29726c18c
Fabricio <fab.oliver.e@gmail.com>
2026-01-05 23:25:32 -0400

    fix: remove overflow property from trust-logos-wrapper and adjust logo array in TrustLogos component

--- src/components/sections/TrustLogos.jsx
+++ src/components/sections/TrustLogos.jsx
@@ -3,8 +3,10 @@
 const TrustLogos = () => {
   const logos = [
     { src: "https://upload.wikimedia.org/wikipedia/commons/4/4d/OpenAI_Logo.svg", alt: "OpenAI" },
+     { src: "https://avatars.githubusercontent.com/u/108554348?s=200&v=4", alt: "Base" },
     { src: "https://upload.wikimedia.org/wikipedia/commons/9/96/Microsoft_logo_%282012%29.svg", alt: "Microsoft" },
-    { src: "https://cryptologos.cc/logos/avalanche-avax-logo.svg", alt: "Avalanche" }
+    { src: "https://cryptologos.cc/logos/avalanche-avax-logo.svg", alt: "Avalanche" },
+   
   ];
 
   return (
